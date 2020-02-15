f2a8faebbe38c052adb0925fc400d6cc8876b597
LevonPapyan <levonlevon31@gmail.com>
2020-02-15 17:02:43 +0400

    first commit

---
+++ src/main/java/am/developway/serverp/ServerpApplication.java
@@ -0,0 +1,13 @@
+package am.developway.serverp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/am/developway/serverp/ServerpApplicationTests.java
@@ -0,0 +1,13 @@
+package am.developway.serverp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
